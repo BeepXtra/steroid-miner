25f86b1ccb58cca1f1b64b3d2c4b08451effd7d5
Angelos Hadjiphilippou <angelos@exevior.com>
2019-06-19 19:26:49 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 url='https://www.beepxtra.com/steroid-miner.json'
+#Get latest version
 newversion=$(
         curl -s "$url" | egrep -m 1 '"version"' | awk -F '"' '{ print $4 }'
 )
+#get upgrade command
 cmd=$(
         curl -s "$url" | egrep -m 1 '"cmd"' | awk -F '"' '{ print $4 }'
 )
+#current installed version
 currentversion=$(cat 'version.txt')
 
-echo $newversion > 'version.txt'
-#echo $newversion
-#echo $cmd
+#Check and execute upgrade if new version
+if($newversion > $currentversion){
+        eval $cmd
+        echo $newversion > 'version.txt'
+}
+
 exit;
