a96422b4727440409765da199920efbea02dee7e
Angelos Hadjiphilippou <angelos@exevior.com>
2019-06-22 17:21:09 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-#v1.02.2
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-SHELL=/bin/sh
+#v1.02.1
+LANG=en_GB.UTF-8
+XDG_VTRN=1
+XVD_SESSION_ID=c1
+HUSHLOGIN=FALSE
+USER=pi
+PWD=/home/pi
+HOME=/home/pi
+PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+SHELL=/bin/bash
+TERM=linux
+SHLVL=1
+XDG_SEAT=seat0
+LOGNAME=pi
+XDG_RUNTIME_DIR=/run/user/1000
 
 url='https://www.beepxtra.com/steroid-miner.json'
 newversion=$(
