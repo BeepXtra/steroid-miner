3a1a2126f7a6c7669c4053e68aabb444e56f2645
Angelos Hadjiphilippou <angelos@exevior.com>
2019-09-10 13:22:39 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #v1.02.1
 wget -q --spider http://google.com
-
+sudo chmod 0755 /home/beep-email.txt
 if [ $? -eq 0 ]; then
     echo "Internet connection is active"
 else
     echo "No connection. Exiting"
     exit;
 fi
-url='https://www.beepxtra.com/steroid-miner.json'
+url='https://www.beepxtra.com/steroid-miner.php'
 newversion=$(
         curl -s "$url" | egrep -m 1 '"version"' | awk -F '"' '{ print $4 }'
 )
@@ -20,7 +20,7 @@ currentversion=$(cat 'version.txt')
 update=$((newversion-currentversion))
 #echo $update
 #exit;
-
+beepacct=$(head -n 1 /home/beep-email.txt)
 if [ "$update" -gt 0 ]
 then
         echo "Upgrade necessary"
