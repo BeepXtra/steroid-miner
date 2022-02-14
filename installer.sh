254899a73b8081cb3e5d00884f73f0ad03fab3c1
Chrysostomos Pogiatzis <33746884+ChrysostomosPogiatzis@users.noreply.github.com>
2022-02-14 16:47:12 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@
 sudo apt install tasksel
 sudo tasksel install lamp-server
 sudo apt-get install php7.4-gmp
+sudo apt-get install net-tools
 echo "What is the email of the beep account for this miner?"
 read varname
 echo "setting up mining account for " $varname
